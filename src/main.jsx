a89ecbc914bb08f6c82718ded02512d4479a879c
Teeka <teekaeyad@gmail.com>
2026-09-04 01:02:32 +0200

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import berniqMark from "./assets/berniq-mark.png";
 import "./styles/global.css";
+
+const favicon = document.querySelector('link[rel="icon"]') ?? document.createElement("link");
+favicon.rel = "icon";
+favicon.href = berniqMark;
+document.head.appendChild(favicon);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
